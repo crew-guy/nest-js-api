8e804154f3ee98115c6770a77e350b24cb6e6a97
ankit <ankit.sanghavi87@gmail.com>
2021-10-25 11:57:12 +0530

    course fin

--- src/common/middleware/validStudent.ts
+++ src/common/middleware/validStudent.ts
@@ -3,13 +3,14 @@ import { NextFunction, Request, Response } from "express";
 import { students } from "src/db";
 
 @Injectable()
-export class ValidStudentMiddleware implements NestMiddleware{
-    use(req: Request, res: Response, next: NextFunction) {
-        const studentId = req.params.studentId;
-        const student = students.some((student) => student.id === studentId)
-        if (!studentId) {
-            throw new HttpException("student not found", 400)
-        }
-        next()
+export class ValidStudentMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    console.log("this middleware was called");
+    const studentId = req.params.studentId;
+    const student = students.some((student) => student.id === studentId);
+    if (!student) {
+      throw new HttpException("student not found", 400);
     }
+    next();
+  }
 }
--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -1,5 +1,10 @@
 import { StudentController } from "src/student/student.controller";
-import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod
+} from "@nestjs/common";
 import { StudentService } from "./student.service";
 import { ValidStudentMiddleware } from "src/common/middleware/validStudent";
 
@@ -8,8 +13,15 @@ import { ValidStudentMiddleware } from "src/common/middleware/validStudent";
   providers: [StudentService],
   exports: [StudentService]
 })
-export class StudentModule implements NestModule{
-    configure(consumer:MiddlewareConsumer) {
-        consumer.apply(ValidStudentMiddleware);
-    }
+export class StudentModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(ValidStudentMiddleware).forRoutes({
+      path: "/students/:studentId",
+      method: RequestMethod.GET
+    });
+    consumer.apply(ValidStudentMiddleware).forRoutes({
+      path: "/students/:studentId",
+      method: RequestMethod.PUT
+    });
+  }
 }
