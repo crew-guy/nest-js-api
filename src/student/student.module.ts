50929c94de16c02a25d6ac1c441b06b13d02bc18
ankit <ankit.sanghavi87@gmail.com>
2021-10-25 11:46:20 +0530

    applied middleware

--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -1,10 +1,15 @@
 import { StudentController } from "src/student/student.controller";
-import { Module } from "@nestjs/common";
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
 import { StudentService } from "./student.service";
+import { ValidStudentMiddleware } from "src/common/middleware/validStudent";
 
 @Module({
   controllers: [StudentController],
   providers: [StudentService],
   exports: [StudentService]
 })
-export class StudentModule {}
+export class StudentModule implements NestModule{
+    configure(consumer:MiddlewareConsumer) {
+        consumer.apply(ValidStudentMiddleware);
+    }
+}
